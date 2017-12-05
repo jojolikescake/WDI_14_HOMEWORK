ed06c2b6746100aeebb1796da5a643281989d404
jojolikescake <jojolikescake@gmail.com>
2017-12-06 09:00:29 +1100

    homework submitted. Sorry for poor effort, haven't been having a good time with my mental health the last couple of days. Please come see me if you need to chat.

--- Joanna Phillips/wk01/4-thu/script.js
+++ Joanna Phillips/wk01/4-thu/script.js
@@ -14,50 +14,24 @@
 
 
 
-var linemap= {
+var linemap= [["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"],
+["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong","Tooronga"], 
+["Southern Cross", "Richmond", "South Yarra", "Prahran","Windsor"]]
 
-Alamein: ["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"],
-Glen_Waverly: ["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong","Tooronga"],
-Sandringham: ["Southern Cross", "Richmond", "South Yarra", "Prahran","Windsor"]
-}
 
 //run through the stations from the origin stop to the destination stop, listing how many stops there are in between
 
-destination= "Windsor"
-origin="Melbourne Central"
-dline="Glen_Waverly"
-oline="Sandringham"
+var destination= prompt("What is your destination?")
+var origin=prompt("what is your origin?")
 document.write("Origin: "+origin+"<br/>")
 
 document.write("Destination: "+destination+"<br/>")
 
- 
-if (oline===dline){
-
- if (linemap[dline].indexOf(destination) > linemap[oline].indexOf(origin)){
-  //train is running forwards through the array
-
-  for (var i = linemap[oline].indexOf(origin); i <= linemap[oline].indexOf(destination);i++){
- document.write(linemap[oline][i]+"--->")
+for (i=0;i<linemap.length;i++){
+  for (j=0;j< linemap[i].length;j++){
+if (linemap[i][j].indexOf(origin)===
+  linemap[i][j].indexOf(destination)){
+  console.log("line is the same")
+ }
 }
-
-document.write("<br/> "+(linemap[oline].indexOf(destination)-linemap[oline].indexOf(origin))+" stops total")
-} else{
-    //train is running backwards through the array
-   var linereverse = linemap[oline].reverse()
-    //reverse the line order
-    for (var i = linereverse.indexOf(origin); i <= linereverse.indexOf(destination);i++){
-    document.write(linereverse[i]+"----> ")
-   
-  }
-   document.write("<br/> "+Math.abs(linereverse.indexOf(destination)-linereverse.indexOf(origin))+" stops total")
-}
-}
-
-else { 
-
-  //this is the bit im confused by, if the two lines arent the same, i want it to loop through until it reaches richmond, then change lines
-  for (var i = linemap[oline].indexOf(origin); i === linemap[oline].indexOf("Richmond");i++){
-     
-  }
 }
