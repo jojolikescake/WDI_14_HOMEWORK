772bfb924053d56dc93908af25539da9149fd24c
jojolikescake <jojolikescake@gmail.com>
2017-12-10 10:57:58 +1100

    homework submitted

--- Joanna Phillips/wk01/4-thu/script.js
+++ Joanna Phillips/wk01/4-thu/script.js
@@ -14,24 +14,78 @@
 
 
 
-var linemap= [["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"],
-["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong","Tooronga"], 
-["Southern Cross", "Richmond", "South Yarra", "Prahran","Windsor"]]
+var lineMap= {
 
+Alamein: ["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"],
+Glen_Waverly: ["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong","Tooronga"],
+Sandringham: ["Southern Cross", "Richmond", "South Yarra", "Prahran","Windsor"]
+}
 
 //run through the stations from the origin stop to the destination stop, listing how many stops there are in between
 
-var destination= prompt("What is your destination?")
-var origin=prompt("what is your origin?")
-document.write("Origin: "+origin+"<br/>")
+var destination= "Flagstaff"
+var origin="Melbourne Central"
+var lines = Object.getOwnPropertyNames(lineMap)
+var stations = Object.values(lineMap)
+var orgline = "Glen_Waverly"
+var destLine = "Glen_Waverly"
 
-document.write("Destination: "+destination+"<br/>")
 
-for (i=0;i<linemap.length;i++){
-  for (j=0;j< linemap[i].length;j++){
-if (linemap[i][j].indexOf(origin)===
-  linemap[i][j].indexOf(destination)){
-  console.log("line is the same")
- }
+
+
+function sameLineOrDifferent (){
+
+  if (orgLine===destLine){
+    sameLine()
+  }
+
+  else {
+    differentLines()
+  }
+
 }
+
+function isItSameLine(){
+
+  // im trying to figure out how i would determine if they were on the same line if the lines werent hardcoded? 
+  // Any ideas? 
+
+for (const prop in lineMap){
+  console.log(lineMap[prop])
+
+  for (i=0;i<lineMap[prop].length;i++){
+    
+    }
+  }
+}
+
+
+function sameLine(){
+  if (lineMap[orgLine].indexOf(origin) < lineMap[destLine].indexOf(destination)){
+    GoForwards()
+  }
+
+  else {
+    GoBack()
+  }
+}
+ 
+function differentLines(){
+console.warn('differentLines has not been coded yet')
+}
+
+function GoForwards (){
+  for (i=0;)
+
+console.warn('GoForwards has not been coded yet')
+}
+
+function GoBack (){
+  console.warning('GoBack has not been coded yet')
+
+}
+
+function PrintStationList(){
+  console.warn('PrintStationList has not been coded yet')
+
 }
