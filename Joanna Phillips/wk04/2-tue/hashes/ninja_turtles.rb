8652f44e442fea5ff438f60156d4248fb177f631
jojolikescake <jojolikescake@gmail.com>
2017-12-19 21:21:33 +1100

    finished ninja turtles

--- Joanna Phillips/wk04/2-tue/hashes/ninja_turtles.rb
+++ Joanna Phillips/wk04/2-tue/hashes/ninja_turtles.rb
@@ -1,7 +1,16 @@
 #Initialize a hash called ninja_turtle with the properties name, weapon and radical. They should have values of "Michelangelo", "Nunchuks" and true respectively.
 
 ninja_turtle = {
-  name:"Michelangelo"
-  weapon:"Nunchuks" 
-  radical:true
+  name:"Michelangelo",
+  weapon:"Nunchuks", 
+  radical:true,
+  age:35,
 }
+
+ninja_turtle.delete(:radical)
+
+ninja_turtle[:pizza_toppings] = ["cheese", "pepperoni", "peppers"]
+
+ninja_turtle[:pizza_toppings][0]
+
+keys_array = ninja_turtle.keys
